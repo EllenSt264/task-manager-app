35a860fd226d23973b8eb07ba83a8792e69ccc98
EllenSt264 <stafford.ellen@protonmail.com>
2021-01-23 14:00:41 +0000

    Add Delete Category functionality

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ def add_category():
     return render_template("add_category.html")
 
 
-@app.route("/edit_categoy/<category_id>", methods=["GET", "POST"])
+@app.route("/edit_category/<category_id>", methods=["GET", "POST"])
 def edit_category(category_id):
     if request.method == "POST":
         submit = {
@@ -178,9 +178,16 @@ def edit_category(category_id):
         mongo.db.categories.update({"_id": ObjectId(category_id)}, submit)
         flash("Category Successfully Updated")
         return redirect(url_for("get_categories"))
-        
+
     category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
     return render_template("edit_category.html", category=category)
+
+
+@app.route("/delete_category/<category_id>)")
+def delete_category(category_id):
+    mongo.db.categories.remove({"_id": ObjectId(category_id)})
+    flash("Category Sucessfully Deleted")
+    return redirect(url_for("get_categories"))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
